0b7ac2fbb498d51d5cb9496a065e31da2b071eac
Sven Helmberger <fforw@gmx.de>
2020-03-07 23:21:34 +0100

    Import old project
    Update dependencies

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react"
+import ReactDOM from 'react-dom'
+import { Canvas, useFrame } from "react-three-fiber"
+// noinspection ES6UnusedImports
+import STYLE from "./style.css"
+
+const VOLUME = 8;
+
+const randomSize = () => {
+    const out = new Array(3);
+
+    const w = Math.round((1 + Math.random()) * 10)/10;
+    const h = Math.round((1 + Math.random()) * 10)/10;
+    const d = VOLUME / (w * h);
+    out[0] = w;
+    out[1] = h;
+    out[2] = d;
+
+    console.log("SIZE: " + out.join(" x "));
+
+    return out;
+};
+
+function Thing() {
+
+    const [size, setSize] = useState(randomSize);
+
+    const ref = useRef();
+    useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+    return (
+        <mesh
+            ref={ref}
+            onClick={e => setSize(randomSize()) }
+            onPointerOver={e => console.log('hover')}
+            onPointerOut={e => console.log('unhover')}>
+            <boxBufferGeometry attach="geometry" args={ size } />
+            <meshNormalMaterial attach="material" />
+        </mesh>
+    )
+}
+
+ReactDOM.render(
+    <Canvas>
+        <Thing />
+    </Canvas>,
+    document.getElementById('root')
+)
